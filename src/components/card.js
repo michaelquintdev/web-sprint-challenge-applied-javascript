b66e55efdb54a642dbba1ebff427e031ff8dcf8b
michaelquintdev <michaelquintdev@gmail.com>
2021-04-09 17:02:16 -0500

    Done with project, I want to go back and make it look more clean but results are in

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,6 +19,36 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+
+  
+  const div = document.createElement("div");
+  const divHeadline = document.createElement("div");
+  const divAuthor =  document.createElement("div");
+  const divImg = document.createElement("div");
+  const img = document.createElement("img");
+  const span = document.createElement("span");
+
+  div.classList.add("card");
+  divHeadline.classList.add("headline");
+  divAuthor.classList.add("author");
+  divImg.classList.add("img-container");
+  
+  divHeadline.textContent = article.headline;
+  img.src = article.authorPhoto;
+  span.textContent = article.authorName;
+
+  div.appendChild(divHeadline);
+  div.appendChild(divAuthor);
+  divAuthor.appendChild(divImg);
+  divImg.appendChild(img);
+  divAuthor.appendChild(span);
+
+  div.addEventListener("click", click => {
+    console.log(divHeadline);
+  })
+
+  return div;
+
 }
 
 const cardAppender = (selector) => {
@@ -28,6 +60,42 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+
+  const element = document.querySelector(selector);
+  axios
+  .get("https://lambda-times-api.herokuapp.com/articles")
+  .then(result => {
+    console.log(result);
+    const bootstrapObj = result.data.articles.bootstrap;
+    const javascriptObj = result.data.articles.javascript;
+    const jqueryObj = result.data.articles.jquery;
+    const nodeObj = result.data.articles.node;
+    const technologyObj = result.data.articles.technology;
+
+    bootstrapObj.forEach(item => {
+      const newCard = Card(item);
+      element.appendChild(newCard);
+    })
+    javascriptObj.forEach(item => {
+      const newCard = Card(item);
+      element.appendChild(newCard);
+    })
+    jqueryObj.forEach(item => {
+      const newCard = Card(item);
+      element.appendChild(newCard);
+    })
+    nodeObj.forEach(item => {
+      const newCard = Card(item);
+      element.appendChild(newCard);
+    })
+    technologyObj.forEach(item => {
+      const newCard = Card(item);
+      element.appendChild(newCard);
+    })
+  })
+  .catch(error => {
+    console.log(error);
+  })
 }
 
 export { Card, cardAppender }
